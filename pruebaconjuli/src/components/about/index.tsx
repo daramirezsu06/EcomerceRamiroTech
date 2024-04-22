0772a402697e1de0bbca1f441ce047154e0ad106
daramirezsu06 <daramirezsu06@gmail.com>
2024-04-22 11:47:52 -0500

    avances generales

--- pruebaconjuli/src/components/about/index.tsx
+++ pruebaconjuli/src/components/about/index.tsx
@@ -1,18 +1,18 @@
-import React from "react";
-
 function About(): React.ReactElement {
   return (
     <>
       <div className="flex flex-col items-center justify-center text-gray-900 py-10 px-4 sm:px-6 lg:px-8 ">
         <div className="max-w-4xl w-full bg-white p-8 rounded-lg shadow-lg text-gray-900">
-          <h2 className="text-3xl font-extrabold text-center mb-6">About Us</h2>
+          <h2 className="text-3xl font-extrabold text-center mb-6">
+            About RamiroTech
+          </h2>
           <div className="space-y-6">
             <p className="text-lg">
-              Welcome to TechVibe, your go-to destination for the latest and
+              Welcome to RamiroTech, your go-to destination for the latest and
               greatest in technology!
             </p>
             <p className="text-lg">
-              At TechVibe, we are passionate about bringing you the best
+              At RamiroTech, we are passionate about bringing you the best
               selection of gadgets, electronics, and accessories. Whether you're
               a tech enthusiast, a gaming guru, or just looking for the perfect
               gift, we've got you covered.
@@ -31,8 +31,8 @@ function About(): React.ReactElement {
               receive exclusive offers and discounts.
             </p>
             <p className="text-lg">
-              At TechVibe, innovation is at the heart of everything we do. Stay
-              tuned for exciting updates, new arrivals, and much more!
+              At RamiroTech, innovation is at the heart of everything we do.
+              Stay tuned for exciting updates, new arrivals, and much more!
             </p>
           </div>
         </div>
