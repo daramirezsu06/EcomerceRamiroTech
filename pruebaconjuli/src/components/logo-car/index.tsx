ad221d7fa3fa668dc2ef8ab7ab1d57748edc6563
daramirezsu06 <daramirezsu06@gmail.com>
2024-04-22 16:00:01 -0500

    avances generales

--- pruebaconjuli/src/components/logo-car/index.tsx
+++ pruebaconjuli/src/components/logo-car/index.tsx
@@ -4,21 +4,60 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 import { useLoginContext } from "../Context";
 export const LogoCar: React.FC = (): React.ReactElement => {
+  const [scrollY, setScrollY] = useState(0);
+  const [isFixed, setIsFixed] = useState(false);
   const { total } = useLoginContext();
 
-  return (
-    <Link href="/cart">
-      <div className="flex items-center gap-2 ">
-        {total == 0 ? (
-          <p>{total}</p>
-        ) : (
-          <p className="animate-bounce">{total}</p>
-        )}
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollY(window.scrollY);
+      if (window.scrollY > 100) {
+        setIsFixed(true);
+      } else {
+        setIsFixed(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [scrollY]);
 
-        <div>
-          <RiShoppingCartLine color="#ffffff" />
+  return (
+    <>
+      {isFixed ? (
+        <div className="fixed top-10 right-10 rounded-full bg-red-500 p-4 ">
+          <Link href="/cart">
+            <div className="flex items-center gap-2 ">
+              {total == 0 ? (
+                <p>{total}</p>
+              ) : (
+                <p className="animate-bounce">{total}</p>
+              )}
+
+              <div>
+                <RiShoppingCartLine color="#ffffff" />
+              </div>
+            </div>
+          </Link>
         </div>
-      </div>
-    </Link>
+      ) : (
+        <div>
+          <Link href="/cart">
+            <div className="flex items-center gap-2 ">
+              {total == 0 ? (
+                <p>{total}</p>
+              ) : (
+                <p className="animate-bounce">{total}</p>
+              )}
+
+              <div>
+                <RiShoppingCartLine color="#ffffff" />
+              </div>
+            </div>
+          </Link>
+        </div>
+      )}
+    </>
   );
 };
